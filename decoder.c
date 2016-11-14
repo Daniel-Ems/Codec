3df6003d51f863a26cf97f8bfe062b48aaacee9f
Daniel Ems <danielems3@outlook.com>
2016-11-14 13:58:30 -0500

    Add pseudo code to decoder for when i start programming

--- decoder.c
+++ decoder.c
@@ -3,4 +3,51 @@
 int main()
 {
 
-	1) create a .c file to push to git hub to create my repsitory. 
+	//1) Add the ability to take two arguments (1."Encode" 2."Decode")
+	//2) "Decode" will have one additional argument, a file to be 		//    decoded.
+	//3) "Encode" Will have two additional arguments, a file to be 
+	//    encoded, and a file to store the encoded file in.
+	//4) Open a the first argument for reading. 
+	//5) Identify the file contains pcaps (strstr for d4 c3 b2 a1)
+	/* 
+	Decoding guidelines, find a way to make it to the zerg 				packet. 
+	-> 1st -> 4bits containing the version
+			Version -> only uses version 1.
+	-> 2nd -> 4bits containing the type
+			Type -> defines the type of payload
+					0) Message
+					1) Status of Zerg
+					3) Command Instruction
+					4) GPS Data
+	-> 3rd -> 3bytes containing the total length
+			Total length -> length of the packet in bytes including
+							fized header
+	-> 4th -> 2bytes containing the Destination Zerg ID
+	-> 5th -> 2bytes containing the source zerg ID
+	-> 6th -> 4bytes containing the sequence ID
+			Sequence ID -> a increment in number of packets passed
+							between the same source and destination
+	-> 7th -> from the 12byte on is the Message Payload
+		Payload's
+		0) Message -> just a message starting at the 12th byte
+		1) Status Payload -> 3bytes of Hit points
+						  -> 1byte of Armor
+						  -> 3bytes of Max hit points 
+						  -> 1byte of type
+						  -> 4bytes of speed
+						  -> the remainder is the name
+		2) Command Payload -> 2bytes of command 
+						   -> 2bytes of parameter 1
+						   -> the reaminder is paramter 2
+		3) GPS Data Payload -> 4bytes Longitude
+							-> 4bytes Latitude
+							-> Altitude
+							-> Bearing
+							-> Speed
+							-> Accuracy
+	*/
+
+
+
+
+		
