7e624d85a0909177293daf655f1c0085119cc2de
Daniel Ems <danielems3@outlook.com>
2016-11-15 16:19:28 -0500

    Add a decode message function to my .c

--- structs.c
+++ structs.c
@@ -1,26 +1,8 @@
 #include "structs.h"
 
-	struct pcap_file_header values;
-	fread(&values, sizeof(values), 1, decode_file);
-
-	struct pcap_packet_header pcap_header;
-	fread(&pcap_header, sizeof(pcap_header), 1, decode_file);
-
-	struct ethernet_header frame;
-	fread(&frame, sizeof(frame), 1, decode_file);
-
-	struct ipv4_header contents;
-	fread(&contents, sizeof(contents), 1, decode_file);
-
-	struct udp_header udp;
-	fread(&udp, sizeof(udp), 1, decode_file);
-
-	struct zerg_header message;
-	fread(&message, sizeof(message), 1, decode_file);
-
-char *decode_message(int total, FILE decode_file)
+char *decode_message(int total, FILE *decode_file)
 {
-		char *message = malloc(total-zerg_header_size);
-		fread(&message, sizeof(message), 1, decode_file);
+		char *message = malloc(total - zerg_header_length);
+		fread(message, total-zerg_header_length, 1, decode_file);
 		return message;
 }
