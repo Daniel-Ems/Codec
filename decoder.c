1fb532270afe8219d3b22b7813f8829b14ca5ba4
Daniel Ems <danielems3@outlook.com>
2016-11-15 14:48:40 -0500

    Move structs to a .h file, and bit wise to access the first three fields of the zerg header

--- decoder.c
+++ decoder.c
@@ -1,70 +1,14 @@
 #include <stdio.h>
 #include <sysexits.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
+
+#include "structs.h"
+
 int main(int argc, char *argv[])
 {
-	const char *packet_id;
+
 	FILE *decode_file;
 
-	//TODO: Make this as a typdef and move to a header file
-	struct pcap_file_header{ 
-		uint32_t file_type;
-		uint32_t major_version : 16;
-		uint32_t minor_version : 16;
-		uint32_t gmt_offset;
-		uint32_t accuracy_delta;
-		uint32_t maximum_length;
-		uint32_t link_layer;
-	};
-
-	struct pcap_packet_header{
-		uint32_t unix_epoch; 
-		uint32_t epoch_microseconds;
-		uint32_t capture_length;
-		uint32_t packet_length;
-	};
-
-	struct ethernet_header{
-		char d_mac[6];
-		char s_mac[6];
-		uint16_t type;
-	};
-
-	struct ipv4_header{
-		uint32_t version : 4;
-		uint32_t ihl : 4;
-		uint32_t dscp : 8;
-		uint32_t total_length : 16;
-		uint32_t id : 16;
-		uint32_t flags : 4;
-		uint32_t offset : 12;
-		uint32_t ttl : 8;
-		uint32_t protocol : 8;
-		uint32_t checksum : 16;
-		uint32_t s_ip;
-		uint32_t d_ip;
-	};
-
-	struct udp_header{
-		uint32_t s_port : 16;
-		uint32_t d_port : 16;
-		uint32_t length : 16;
-		uint32_t checksum : 16;
-	};
-
-	struct zerg_header{
-		uint32_t version : 4 ;
-		uint32_t type : 4;
-		uint32_t total : 24;
-		uint32_t source : 16;
-		uint32_t dest : 16;
-		uint32_t id;
-		char payload[32];
-	};
- 
-	if(argc < 1)
+	if(argc < 2)
 	{
 		printf("Please provide a file to be decoded\n");
 		return EX_USAGE;
@@ -74,6 +18,21 @@ int main(int argc, char *argv[])
 
 	struct pcap_file_header values;
 	fread(&values, sizeof(values), 1, decode_file);
+
+	struct pcap_packet_header pcap_header;
+	fread(&pcap_header, sizeof(pcap_header), 1, decode_file);
+
+	struct ethernet_header frame;
+	fread(&frame, sizeof(frame), 1, decode_file);
+
+	struct ipv4_header contents;
+	fread(&contents, sizeof(contents), 1, decode_file);
+
+	struct udp_header udp;
+	fread(&udp, sizeof(udp), 1, decode_file);
+
+	struct zerg_header message;
+	fread(&message, sizeof(message), 1, decode_file);
 
 	//TODO: Remove debugging pruint32_t statements
 	printf("File Header -> size:%zd\n", sizeof(values));
@@ -85,36 +44,25 @@ int main(int argc, char *argv[])
 	printf("%x\n", values.maximum_length);
 	printf("%x\n", values.link_layer);
 
-	struct pcap_packet_header pcap_header;
-	fread(&pcap_header, sizeof(pcap_header), 1, decode_file);
-
 	printf("Pcap packet Header, size:%zd\n", sizeof(pcap_header));
 	printf("epoch %x\n",pcap_header.unix_epoch);
 	printf("microseconds %x\n", pcap_header.epoch_microseconds);
 	printf("capture_length %x\n", pcap_header.capture_length);
 	printf("packet_length %x\n", pcap_header.packet_length);
 
-	struct ethernet_header frame;
-	fread(&frame, sizeof(frame), 1, decode_file);
-
 	printf("Ethernet Frame, size:%zd\n", sizeof(frame));
 	printf("destination %x\n", frame.d_mac);
 	printf("Source %x\n", frame.s_mac);
 	printf("type %x\n", frame.type);
 
-
-
-	struct ipv4_header contents;
-	fread(&contents, sizeof(contents), 1, decode_file);
-
 	printf("Ipv4 header, size:%zd\n", sizeof(contents));
 	printf("version %x\n", contents.version);
-	printf("ihl %x\n", contents.ihl);
+	//printf("ihl %x\n", contents.ihl);
 	printf("dscp %x\n", contents.dscp);
 	//printf("ecn %x\n", contents.ecn);
 	printf("total_length %x\n", contents.total_length);
 	printf("id %x\n", contents.id);
-	printf("flags %x\n", contents.flags);
+	//printf("flags %x\n", contents.flags);
 	printf("offset %x\n", contents.offset);
 	printf("ttl %x\n", contents.ttl);
 	printf("protocol %x\n", contents.protocol);
@@ -122,27 +70,28 @@ int main(int argc, char *argv[])
 	printf("s_ip %x\n", contents.s_ip);
 	printf("d_ip %x\n", contents.d_ip);
 
-	struct udp_header udp;
-	fread(&udp, sizeof(udp), 1, decode_file);
-
 	printf("UDP Header, size:%zd\n", sizeof(udp));
 	printf("s_port %x\n", udp.s_port);
 	printf("d_port %x\n", udp.d_port);
-	printf("length %x\n", udp.length);
+	printf("length %d\n", ntohs(udp.length));
 	printf("checksum %x\n", udp.checksum);
 
-	struct zerg_header message;
-	fread(&message, sizeof(message), 1, decode_file);
+	int type = message.version & 0x0f;
+	int total = message.version >> 24;
+	message.version = message.version >> 28; 
 
+	//int version = message.type;
 	printf("zerg header, size:%zd\n", sizeof(message));
 	printf("Version %x\n", message.version);
-	printf("type %x\n", message.type);
-	printf("total %x\n", message.total);
+	printf("type %d\n", type);
+	printf("total %d\n", total);
 	printf("source %x\n", message.source);
 	printf("destination %x\n", message.dest);
-	printf("id %x\n", message.id);
+	printf("id %x\n", ntohs(message.id));
 	printf("payload %s\n", message.payload);
+	//printf("version %d\n", version);
 
+	fclose(decode_file);
 }
 
 
