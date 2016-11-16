343d1a66ebd2a86b909f0d1280534efd1c7dfbff
Daniel Ems <danielems3@outlook.com>
2016-11-16 16:32:43 -0500

    Add a union to my structs.c in order to turn an int into a float

--- structs.c
+++ structs.c
@@ -1,5 +1,5 @@
 #include "structs.h"
-
+#include <arpa/inet.h>
 
 
 
@@ -23,9 +23,24 @@ void messages(union payload *zerg)
 
 void status(union payload *zerg)
 {
-		printf("%x\n", zerg-> zerg.hit_armor);
-		printf("%x\n", zerg-> zerg.max_type);
-		printf("%x\n", zerg-> zerg.speed);
-		printf("%s\n", zerg-> zerg.name);
+		zerg -> zerg.hit_armor = zerg -> zerg.hit_armor >> 16;
+		int32_t type = ntohl(zerg -> zerg.max_type) & 0xf;
+		zerg -> zerg.max_type = ntohl(zerg -> zerg.max_type) >> 8;
+		//zerg -> zerg.speed = zerg -> zerg.speed;
+
+		//TODO: put in function, will need for longitude and latitude
+ 		union converter{
+			float speed;
+			uint32_t test;
+		};
+		union converter speed_test; 
+		speed_test.test = ntohl(zerg->zerg.speed);
+		float speedy = speed_test.speed;
+
+		printf("Hit %x\n", zerg-> zerg.hit_armor);
+		printf("type %x\n", type);
+		printf("max %x\n", zerg-> zerg.max_type);
+		printf("speed %f\n",speedy);
+		printf("name %s\n", zerg-> zerg.name);
 		free(zerg);
 } 
