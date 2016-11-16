93e7377cdc4433a62f946d588d81d12782aba347
Daniel Ems <danielems3@outlook.com>
2016-11-16 12:54:04 -0500

    Add and remove debugging checks t ofix segfault issue.

--- structs.c
+++ structs.c
@@ -5,14 +5,18 @@
 
 union payload *struct_init(int total, FILE *decode_file)
 {
-		union payload *memory = malloc(total - zerg_header_length);
-		fread(memory, total-zerg_header_length, 1, decode_file);
+		union payload *memory = calloc((total - zerg_header_length )+1,1);
+		if(!memory){
+			printf("NO MALLOC");
+			}
+		fread(memory, total-zerg_header_length , 1, decode_file);
 		return memory;
 }
 
 void messages(union payload *zerg)
 {
-		printf("%s\n",zerg -> name.message);
+
+		printf("%s\n", zerg -> name.message);
 		free(zerg);
 
 }
