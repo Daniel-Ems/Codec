c432e25caaab9ef37500da00b737ec6a182cf1bb
Daniel Ems <danielems3@outlook.com>
2016-11-16 09:50:37 -0500

    Add a function that mallocs memory to the union

--- structs.c
+++ structs.c
@@ -6,3 +6,12 @@ char *decode_message(int total, FILE *decode_file)
 		fread(message, total-zerg_header_length, 1, decode_file);
 		return message;
 }
+
+
+union payload *struct_init(int total, FILE *decode_file)
+{
+		union payload *memory = malloc(total - zerg_header_length);
+		fread(memory, total-zerg_header_length, 1, decode_file);
+		return memory;
+}
+
