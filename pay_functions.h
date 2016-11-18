c9e09957b42276200ab6caf6a03f3d928b1e55c5
Daniel Ems <danielems3@outlook.com>
2016-11-18 15:55:26 -0500

    Add the function declarations for my payload to my .h

--- pay_functions.h
+++ pay_functions.h
@@ -1,8 +1,18 @@
 #ifndef PAY_FUNCTIONS_H
 #define PAY_FUNCTIONS_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
 #include <arpa/inet.h>
 
-const char *race(uint32_t type);
+extern union payload *zerg;
+void messages(union payload *zerg);
+void stat_payload(union payload *zerg);
+void com_payload(union payload *zerg);
+void gps(union payload *zerg);
+
 
 #endif
