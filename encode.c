3a311a37bc45269b68bb61957dc0188c8e34fc78
Daniel Ems <danielems3@outlook.com>
2016-11-21 13:55:14 -0500

    Add if statement to create a file from the argument

--- encode.c
+++ encode.c
@@ -6,7 +6,15 @@
 
 int main (int argc, char *argv[])
 {
-	
+	FILE *encodeFile;
+
+	if(argc < 2)
+	{
+		printf("Please provide a file to be encoded\n");
+		return EX_USAGE;
+	}else{ 
+		encodeFile = fopen(argv[1], "rb");
+	}
 
 
 }
