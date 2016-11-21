90b4d72da1286a0b5d5d1eb1389700ac7877779b
Daniel Ems <danielems3@outlook.com>
2016-11-21 11:22:11 -0500

    Add basic .c features, work on implementing a loop that will take in file input and out put the value's

--- encode.c
+++ encode.c
@@ -1,9 +1,13 @@
-
+#include <stdio.h>
+#include <sysexits.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <ctype.h>
 
 int main (int argc, char *argv[])
 {
 	FILE *encodeFile;
-
+	char numCheck[20] = {'0','1','2','3','4','5','6','7','8','9'};
 	if(argc < 2)
 	{
 		printf("Please provide a file to be decoded\n");
@@ -12,3 +16,22 @@ int main (int argc, char *argv[])
 		encodeFile = fopen(argv[1], "rb");
 	}
 
+	char *value;
+	char tmpBuff[64];
+	for(size_t count = 0; count < 4; count ++);
+	{
+		fgets(tmpBuff, sizeof(tmpBuff), encodeFile);
+		while(1)
+		{
+			for(size_t num = 0; num < sizeof(numCheck); num++)
+			{
+				value = strstr(numCheck[num], tmpBuff);
+			}
+		}
+
+	printf("%s\n", value);
+
+	}
+
+
+}
