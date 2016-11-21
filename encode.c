3993e57ea5914535a1d93ad894db62ff92b74759
Daniel Ems <danielems3@outlook.com>
2016-11-21 14:15:48 -0500

    Add a loop that checks whether or not a character is a digit or not

--- encode.c
+++ encode.c
@@ -16,5 +16,22 @@ int main (int argc, char *argv[])
 		encodeFile = fopen(argv[1], "rb");
 	}
 
-
+	char tmpBuff [64];
+	char values[64] = "\0";
+	size_t i;
+	size_t a = 0;
+	for(size_t count = 0; count < 4; count++)
+	{
+		fgets(tmpBuff, sizeof(tmpBuff), encodeFile);
+		for (i = 0; i < strlen(tmpBuff); i++)
+		{
+			if(isdigit(tmpBuff[i]))
+			{
+				values[a] = tmpBuff[i];
+				a++;
+			}
+		}
+		printf("%s\n", values);
+		a = 0;
+	}
 }
