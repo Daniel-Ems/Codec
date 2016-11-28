1fbda0bf71c653650b98ed726506a53793f813ee
Daniel Ems <danielems3@outlook.com>
2016-11-28 15:36:31 -0500

    Create a function called getType to sort the types of the payloads

--- encode_function.c
+++ encode_function.c
@@ -14,3 +14,30 @@ int getVal(char *tmpBuff, const char *search)
 		ver = strtol(tmpBuff,NULL, 10);
 		return ver;
 	}
+
+int getType(char *payload)
+	{
+		int type;
+		if(strncmp(payload, "message",7)==0)
+		{
+			type = 0;
+			printf("%d\n", type);
+		}
+		else if(strncmp(payload, "name",4)==0)
+		{
+			type = 1;
+			printf("%x\n", type);
+		}
+		else if(strncmp(payload, "latitude",8)==0)
+		{
+			type = 4;
+			printf("%d\n", type);
+		}
+		else 
+		{
+			type = 3;
+			printf("%d\n", type);
+		}
+		return type;
+	}
+
