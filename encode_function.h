7e6965a8e725c01f4e2188c6ba9356f69180363e
Daniel Ems <danielems3@outlook.com>
2016-11-28 10:42:50 -0500

    Add a .h file for the declarations of my functionis to my encode_functions.c file

---
+++ encode_function.h
@@ -0,0 +1,14 @@
+#ifndef ENCODE_FUNCTION_H
+#define ENCODE_FUNCTION_H
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <sysexits.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int getVal(char *tmpBuff, const char *search);
+
+#endif
