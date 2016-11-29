5d966c2187e59e312a4acffcae538ee99ba95920
Daniel Ems <danielems3@outlook.com>
2016-11-29 09:18:30 -0500

    Make a function that takes in two Buffers and returns the current payload you are working on

--- encode_function.c
+++ encode_function.c
@@ -28,31 +28,57 @@ int getType(char *payload)
 		}
 		else if(strncmp(payload, "latitude",8)==0)
 		{
-			type = 4;
+			type = 3;
 		}
 		else 
 		{
-			type = 3;
+			type = 2;
 		}
 		return type;
 	}
 
 char *getPayload(char *tmpBuff,char *payload)
 	{	
-		int a = 0;
+
 		while(!isalpha(*tmpBuff))
 		{
 			tmpBuff++;
 		}
 		while(isalpha(*tmpBuff))
 		{
-			payload[a] = *tmpBuff;
-			a++;
+			*payload = *tmpBuff;
+			payload++;
 			tmpBuff++;
 		}
+		
 		while(!isalnum(*tmpBuff))
 		{
+			*payload = *tmpBuff;
+			payload++;
 			tmpBuff++;
 		}
+		
 		return tmpBuff;
 	}
+
+char *getPacket(char *tmpBuff, char *payload, size_t fileEnd)
+	{
+		char *tmpCapture = calloc(1,fileEnd + 1);
+		if((payload = strcasestr(tmpBuff, "Version")))
+		{
+			int remainder = payload - tmpBuff;
+
+			for(int a = 0; a < remainder; a++)
+			{
+				tmpCapture[a] = *tmpBuff;
+				tmpBuff++;
+			}
+		}
+		else
+		{
+	 	tmpCapture = tmpBuff;
+		}
+		return tmpCapture;
+	}
+
+
