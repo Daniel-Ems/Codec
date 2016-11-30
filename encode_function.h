eabd14877adf30a54746266a5a1179c404cde4f0
Daniel Ems <danielems3@outlook.com>
2016-11-30 10:11:25 -0500

    Add the declaration for my converter functon to my encode_function.h

--- encode_function.h
+++ encode_function.h
@@ -3,17 +3,25 @@
 
 #define _GNU_SOURCE
 #include <stdio.h>
-#include <sysexits.h>
 #include <sys/stat.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+#include <sysexits.h>
+#include <sys/stat.h>
+
+
 
 int getVal(char **tmpBuff, const char *search);
 int getType(char *payload);
 char *getPayload(char *tmpBuff, char *payload);
 char *getPacket(char *tmpBuff, char *payload, off_t fileEnd);
 void notdigit(char **packetCapture);
+uint32_t convertInt(float tmpNum);
 //char payVals(char **packetCapture, const char *search, off_t fileEnd);
 
 #endif
