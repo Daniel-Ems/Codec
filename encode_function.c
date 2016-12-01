78152b3bcade6c79ad7b51ade8ff3b8cb1b1d52b
Daniel Ems <danielems3@outlook.com>
2016-12-01 15:43:56 -0500

    Move struct print statments out of main into a function in my encode_functions.c

--- encode_function.c
+++ encode_function.c
@@ -136,14 +136,52 @@ void fileHeader(FILE *writeFile)
 		return;
 	}
 
-void ethernetHeader(FILE *writeFile)
+
+
+void writeHeaders(FILE *writeFile, int size)
 	{
+		struct PcapHeader ph;
+		ph.unixEpoch = 0x00000000;
+		ph.epochMicroseconds = 0x00000000;
+		ph.captureLength = htonl(54 + size); 
+		ph.packetLength = 0x00000000;
+
+		fwrite(&ph, sizeof(ph), 1, writeFile);
+
 		struct EthernetFrame ef;
 		ef.d_mac = 0x000000000000;
 		ef.s_mac = 0x111111111111;
 		ef.type = 0x0008; 
 
 		fwrite(&ef, sizeof(ef), 1, writeFile);
-		return;
+
+		struct encodeIpv4 ei;
+		ei.version = 0x45; 
+		ei.dscpEcn = 0x00;
+		ei.total_length = (40 + size);
+		printf("iplength: %d\n", ei.total_length);
+		ei.id = 0x00000000;
+		ei.offset = 0x0016;
+		ei.protocol = 0x1100;
+		ei.checksum = 0x1234;
+		ei.s_ip = 0x87654321;
+		ei.d_ip = 0x12345678;
+
+		fwrite(&ei, sizeof(ei), 1, writeFile);
+
+		struct UdpHeader uh;
+		uh.s_port = 0x1111;
+		uh.d_port = 0xa70e;
+		uh.length = (20 + size);
+		printf("UDPlength%d\n", uh.length);
+		uh.checksum = 0x4444;
+
+		fwrite(&uh, sizeof(uh), 1, writeFile);
 	}
+
+
+
+
+
+
 
