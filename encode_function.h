ffab4e0f793a7d6cb347e196479af2f90c4d301d
Daniel Ems <danielems3@outlook.com>
2016-12-01 10:00:17 -0500

    Add the declaration for my double to 64 converter

--- encode_function.h
+++ encode_function.h
@@ -2,6 +2,8 @@
 #define ENCODE_FUNCTION_H
 
 #define _GNU_SOURCE
+#define _BSD_SOURCE
+#include <endian.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <string.h>
@@ -21,8 +23,9 @@ int getVal(char **tmpBuff, const char *search);
 int getType(char *payload);
 void getPayload(char **packerCapture,int type);
 char *getPacket(char *tmpBuff, char *payload, off_t fileEnd);
-void notdigit(char **packetCapture);
+void notDigit(char **packetCapture);
 uint32_t convertInt(float tmpNum);
+uint64_t convertDoub(double number);
 //char payVals(char **packetCapture, const char *search, off_t fileEnd);
 
 #endif
