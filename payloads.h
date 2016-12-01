eb8cb45a89ee37a9102190619b7850aab30bc3f3
Daniel Ems <danielems3@outlook.com>
2016-12-01 15:48:26 -0500

    Add a struct pointer to my payload function declarations in my payloads.h

--- payloads.h
+++ payloads.h
@@ -1,7 +1,8 @@
 #ifndef payload_H
 #define paylaod_H
 
-void StatusPayload(char *packetCapture, FILE* writeFile);
-void CommandPayload(char *packetCapture, FILE *writeFile);
-void GpsPayload(char *packetCapture, FILE *writeFile);
+void StatusPayload(char *packetCapture, FILE* writeFile, struct EncodeZerg *ez);
+void CommandPayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
+void GpsPayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
+void MessagePayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
 #endif
