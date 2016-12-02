c1174ac74c887efdaa94305b39f81e2e125048a7
Daniel Ems <danielems3@outlook.com>
2016-12-02 10:28:31 -0500

    Update format as per style guide requirements using .indent.pro

--- encode.c
+++ encode.c
@@ -1,4 +1,4 @@
-#define _GNU_SOURCE 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <sysexits.h>
 #include <sys/stat.h>
@@ -12,113 +12,121 @@
 #include "payloads.h"
 
 
-int main (int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-	FILE *encodeFile;
-	FILE *writeFile;
+    FILE *encodeFile;
+    FILE *writeFile;
 
-	struct stat *buf = malloc( sizeof(*buf));
-	stat(argv[1], buf);
+    struct stat *buf = malloc(sizeof(*buf));
 
-	off_t fileEnd = buf->st_size;
-	if(fileEnd ==0)
-	{
-		return EX_USAGE;
-	}
+    stat(argv[1], buf);
 
-	if(argc < 3)
-	{
-		printf("Please provide a file to be encoded, and to be written too\n");
-		return EX_USAGE;
-	}
-	else
-	{ 
-		encodeFile = fopen(argv[1], "rb");
-		if(!encodeFile)
-		{
-			return EX_USAGE;
-		}
-		writeFile = fopen(argv[2], "wb");
-		if(!writeFile)
-		{
-			return EX_USAGE;
-		}
-	}
+    off_t fileEnd = buf->st_size;
 
-	char *tmpBuff = calloc(1, fileEnd+1);
-	char *buffPointer = tmpBuff;
+    if (fileEnd == 0)
+    {
+        return EX_USAGE;
+    }
 
-	for(int count = 0; count < fileEnd ; count++)
-	{
-		tmpBuff[count] = fgetc(encodeFile);
-	}
-	fileHeader(writeFile);
+    if (argc < 3)
+    {
+        printf("Please provide a file to be encoded, and to be written too\n");
+        return EX_USAGE;
+    }
+    else
+    {
+        encodeFile = fopen(argv[1], "rb");
+        if (!encodeFile)
+        {
+            return EX_USAGE;
+        }
+        writeFile = fopen(argv[2], "wb");
+        if (!writeFile)
+        {
+            return EX_USAGE;
+        }
+    }
 
-while(tmpBuff)
-{
+    char *tmpBuff = calloc(1, fileEnd + 1);
+    char *buffPointer = tmpBuff;
 
-	char *payload = calloc(1, fileEnd+1);
-	char *payloadPointer = payload;
+    for (int count = 0; count < fileEnd; count++)
+    {
+        tmpBuff[count] = fgetc(encodeFile);
+    }
+    fileHeader(writeFile);
 
-	int headerValues[4];
-	const char *headerFields[4] = {"Version", "Sequence","From", "To" };
-	int a;
-	for( a = 0; a < 4; a++)
-	{
-		headerValues[a] = getVal(&tmpBuff, headerFields[a]);
-	}
+    while (tmpBuff)
+    {
 
-	char *packetCapture = getPacket(tmpBuff, payload, fileEnd);
-	char *packetPointer = packetCapture;
+        char *payload = calloc(1, fileEnd + 1);
+        char *payloadPointer = payload;
 
-	int type = getType(packetCapture);
-	getPayload(&packetCapture,type);
+        int headerValues[4];
+        const char *headerFields[4] = { "Version", "Sequence", "From", "To" };
+        int a;
 
-	a = 0;
+        for (a = 0; a < 4; a++)
+        {
+            headerValues[a] = getVal(&tmpBuff, headerFields[a]);
+        }
 
-	struct EncodeZerg *ez = malloc(sizeof (*ez));
-	ez -> version = (headerValues[0]);
-	ez -> type  = (type);
-	ez -> source = htons(headerValues[2]);
-	ez -> dest = htons(headerValues[3]);
-	ez -> id = htonl(headerValues[1]);
+        char *packetCapture = getPacket(tmpBuff, payload, fileEnd);
+        char *packetPointer = packetCapture;
 
-		int exit = 0;
-		int typeCase = type;
-		switch(typeCase){
-			case(0):
-				exit = MessagePayload(packetCapture, writeFile, ez);
-				break;
-			case(1):
-				exit = StatusPayload(packetCapture, writeFile,ez);
-				break;
-			case(2):
-				exit = CommandPayload(packetCapture, writeFile,ez);
-				break;
-			case(3):
-				exit = GpsPayload(packetCapture, writeFile, ez);
-				break;
+        int type = getType(packetCapture);
 
-		}
+        getPayload(&packetCapture, type);
 
-	free(packetPointer);
-	free(payloadPointer);
-	free(ez);
+        a = 0;
 
-	if(exit)
-	{
-		free(buffPointer);
-		free(buf);
-		fclose(encodeFile);
-		fclose(writeFile);
-		return EX_USAGE;
-	}
+        struct EncodeZerg *ez = malloc(sizeof(*ez));
 
-	tmpBuff = strcasestr(tmpBuff, "Version");
-}
+        ez->version = (headerValues[0]);
+        ez->type = (type);
+        ez->source = htons(headerValues[2]);
+        ez->dest = htons(headerValues[3]);
+        ez->id = htonl(headerValues[1]);
 
-	free(buffPointer);
-	free(buf);
-	fclose(encodeFile);
-	fclose(writeFile);
+        int exit = 0;
+        int typeCase = type;
+
+        switch (typeCase)
+        {
+        case (0):
+            exit = MessagePayload(packetCapture, writeFile, ez);
+            break;
+        case (1):
+            exit = StatusPayload(packetCapture, writeFile, ez);
+            break;
+        case (2):
+            exit = CommandPayload(packetCapture, writeFile, ez);
+            break;
+        case (3):
+            exit = GpsPayload(packetCapture, writeFile, ez);
+            break;
+
+        }
+
+        free(packetPointer);
+        free(payloadPointer);
+        free(ez);
+
+        if (exit)
+        {
+            free(buffPointer);
+            free(buf);
+            fclose(encodeFile);
+            fclose(writeFile);
+            return EX_USAGE;
+        }
+
+        tmpBuff = strcasestr(tmpBuff, "Version");
+    }
+
+    free(buffPointer);
+    free(buf);
+    fclose(encodeFile);
+    fclose(writeFile);
 }
