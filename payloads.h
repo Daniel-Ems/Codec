f54554c01f6cbe769f185124ef290dfbf8289ae8
Daniel Ems <danielems3@outlook.com>
2016-12-02 13:17:01 -0500

    update indentation for style guide requirements

--- payloads.h
+++ payloads.h
@@ -1,8 +1,11 @@
 #ifndef payload_H
 #define paylaod_H
 
-int StatusPayload(char *packetCapture, FILE* writeFile, struct EncodeZerg *ez);
-int CommandPayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
-int GpsPayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
-int MessagePayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
+int StatusPayload(char *packetCapture, FILE * writeFile,
+                  struct EncodeZerg *ez);
+int CommandPayload(char *packetCapture, FILE * writeFile,
+                   struct EncodeZerg *ez);
+int GpsPayload(char *packetCapture, FILE * writeFile, struct EncodeZerg *ez);
+int MessagePayload(char *packetCapture, FILE * writeFile,
+                   struct EncodeZerg *ez);
 #endif
