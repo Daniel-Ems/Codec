bcf86052a52f4964dade8d103c6e337122c9ade5
Daniel Ems <danielems3@outlook.com>
2016-12-02 09:46:16 -0500

    Add fwrite checks to .h

--- payloads.h
+++ payloads.h
@@ -1,8 +1,8 @@
 #ifndef payload_H
 #define paylaod_H
 
-void StatusPayload(char *packetCapture, FILE* writeFile, struct EncodeZerg *ez);
-void CommandPayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
-void GpsPayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
-void MessagePayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
+int StatusPayload(char *packetCapture, FILE* writeFile, struct EncodeZerg *ez);
+int CommandPayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
+int GpsPayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
+int MessagePayload(char *packetCapture, FILE *writeFile, struct EncodeZerg *ez);
 #endif
